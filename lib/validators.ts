894a9a6526adf11c0ade52467319197bc0470555
Tausif <tausif@Tausifs-Mac-mini.local>
2025-11-04 01:32:08 +0600

    fixed

--- lib/validators.ts
+++ lib/validators.ts
@@ -11,7 +11,7 @@ export const insertProductSchema = z.object({
   description: z.string().min(3, 'Description must be at least 3 characters'),
   stock: z.coerce.number(),
   images: z.array(z.string()).min(1, 'Product must have at least 1 image'),
-  isFeatures: z.boolean(),
+  isFeatured: z.boolean(),
   banner: z.string().nullable(),
-  price: currency
+  price: z.string()
 })
