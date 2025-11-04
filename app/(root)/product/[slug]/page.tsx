815095d33a20164c16f1531454b5b6e9752dce60
Tausif <tausif@Tausifs-Mac-mini.local>
2025-11-05 02:06:35 +0600

    done

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -1,18 +1,5 @@
-import { getProductBySlug } from '@/lib/actions/product.actions'
-import { notFound } from 'next/navigation'
-import { Badge } from '@/components/ui/badge'
-import { Button } from '@/components/ui/button'
-import { Card, CardContent } from '@/components/ui/card'
-import ProductImage from '@/components/shared/product/product-images'
-
-const ProductDetailsPage = async (props: { params: { slug: string } }) => {
-  const { slug } = props.params
-
-  const product = await getProductBySlug(slug)
-
-  if (!product) notFound()
-
-  return <></>
+const SingleProduct = () => {
+  return <>Single Product</>
 }
 
-export default ProductDetailsPage
+export default SingleProduct
